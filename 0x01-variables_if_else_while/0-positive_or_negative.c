902cd727fd5b9f695e47d7b1eb4148433ff80d79
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-18 17:35:57 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 
-int main(void)     // this the main function which contains void as its condition
+int main(void)     /*this the main function which contains void as its condition */
 {
 	int n;
 
