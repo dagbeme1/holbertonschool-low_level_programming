b3891ad31966ad653306e0af08cf82aede5257ed
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-20 01:35:01 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 
@@ -16,10 +14,10 @@ int main(void)
 	int base10 = '0';
 
 	/* for loop to print out base 10 */
-	for (base10 = 0; base10 < 10; base10++)
-	{
-	putchar(base10);
-	}
+	for (base10 = '0'; base10 <= '9'; base10++)
+
+	putchar(base10);	
+
 	putchar('\n');
 
 	return (0);
