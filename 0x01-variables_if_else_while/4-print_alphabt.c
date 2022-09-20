3f9e86c4e76b1857c5e600447848f2d203eb4e37
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-20 00:35:27 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	/* get the character to be written */
+	char ch = 'a';
+	/* write the character to the standard output */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	if (ch != 'q' && ch != 'e'){
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
