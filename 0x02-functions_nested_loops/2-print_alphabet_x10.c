ba3c6547bbcdb528fa48c0fde6ffb3698f2bb42d
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-20 16:26:04 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * print_alphabet - print lowercase alphabet
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char bet = 'a';
+
+	int tencount;
+	/* for loop */
+	for (tencount = 0; tencount <= 10; tencount++)
+	{
+	for (bet = 'a'; bet <= 'z'; bet++)
+	_putchar(bet);
+	}
+	_putchar('\n');
+
+}
