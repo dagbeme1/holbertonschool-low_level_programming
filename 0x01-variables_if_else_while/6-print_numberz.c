c70f5aa6b0acbc4599b7b5fa5c304e44a49d74a1
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-20 01:14:53 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	/* get the character to be written */
+	int base10;
+
+	/* for loop to print out base 10 */
+	for (base10 = 0; base10 < 10; base10++)
+	{
+	return (base10);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
