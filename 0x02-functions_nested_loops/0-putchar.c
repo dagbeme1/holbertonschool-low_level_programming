586cb4e94c688d8baaef32c395d6954b94e11c3a
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-20 11:23:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	_putchar('-');
+	_putchar('p');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is readme.md for 0x02 functions
