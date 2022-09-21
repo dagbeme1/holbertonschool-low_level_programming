bc063eb772d442bb43789b8ae476fc255db96882
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-20 19:58:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _abs - abs of number
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+
+int _abs(int n)
+{
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        else if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+        else
+        {
+                _putchar('-');
+                return (-1);
+        }
+}
