829dfda89c30d876b9f9e41200f0efbffdb64265
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-25 17:53:05 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,14 @@
 
 /**
  * print_numbers - a function that checks for uppercase character
- * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
 void print_numbers(void)
 {
-int c = 0;
+int c;
 
-for ( c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 
 _putchar(c);
