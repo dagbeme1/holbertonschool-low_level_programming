d5d7642cf710c411f37eb30b7749869b5ff04c21
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-25 17:12:22 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
