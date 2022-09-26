c4d6128c9e6991d80d1f579fc70674e90b06b6f9
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-25 18:44:33 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * _putchar
+ */
 void print_most_numbers(void)
-{ 
+{
 /* get the character to be written */
 int u = 2;
 /* write the character to the standard output */
@@ -11,5 +15,4 @@ _putchar(u);
 
 _putchar('\n');
 
-       
 }
