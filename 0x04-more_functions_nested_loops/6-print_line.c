c03fbd622e81ec44147313d91d917649ab2c49f9
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-09-26 16:43:14 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *@n: integer
+ * _putchar Always newline.
+ */
+
+void print_line(int n)
+{
+int count;
+for (count = n; count > 0; count--)
+_putchar('_');
+
+_putchar('\n');
+
+}
