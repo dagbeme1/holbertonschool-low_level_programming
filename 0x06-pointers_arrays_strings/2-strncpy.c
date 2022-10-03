150efade69109851afa7fc3a85de85777bc401bf
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-10-03 13:26:57 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strncpy - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
