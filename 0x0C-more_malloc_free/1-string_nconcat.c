0215cde706b9d92bbf9541c7b4ad1aa0e31b861b
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-10-17 13:29:15 -0700

    starters

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _strlen - calculate and return string length
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdlib.h>
 
 void *malloc_checked(unsigned int b);
