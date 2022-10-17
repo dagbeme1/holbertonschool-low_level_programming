e4a97d140935e239087a811ac48d0e40b56620ca
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-10-17 13:06:30 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,57 +1,51 @@
-#include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
- */
-
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
- * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
  * @s1: string 1
  * @s2: string 2
- * @n: n bytes to concat from string 2
- * Return: pointer to concatenated string
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	int num, len, i, j;
+	char *p;
+	int strlen1, i, c;
 
-	num = n;
-
-	if (s1 == NULL) /* account for NULL strings */
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0) /* account for negative n bytes */
+
+	strlen1 = (unsigned int)_strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	if (num >= _strlen(s2)) /* account for n too big */
-		num = _strlen(s2);
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	{
+		if (i < strlen1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
+	}
+	p[i] = '\0';
 
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+	return (p);
+}
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-	if (ptr == NULL)
-		return (NULL);
+/**
+ * _strlen - find length of string
+ * @s: string
+ * Return: length of string
+ */
 
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
-		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
+int _strlen(char *s)
+{
+	int i;
 
-	return (ptr);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
