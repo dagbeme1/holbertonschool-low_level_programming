e12136ad892f0c0aa3e5d2cf56b393cbcbda1d65
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-10-21 12:07:57 -0700

    fixed betty to get the checkers working

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,18 +3,15 @@
 #include "dog.h"
 
 /**
- * init_dog - initialize a variable of type `struct dog`
+ * print_dog - initialize a variable of type `struct dog`
  * @d: struct dog
- * @name: string for name
- * @age: integer for age
- * @owner: string for owners name
  */
 
 void print_dog(struct dog *d)
 {
 
-        if (d == NULL) /* validate if d initiated correctly */
-                return;
+	if (d == NULL) /* validate if d initiated correctly */
+		return;
 
 	(d->name == NULL) ? printf("Name: (nil)\n")
 		: printf("Name: %s\n", d->name);
