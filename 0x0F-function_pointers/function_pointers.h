2a6f4ac9de613a09641c6c05346d6a509f4a1491
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-10-21 16:39:15 -0700

    starterss

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#ifndef _FUNCTION_POINTERS
-#define _FUNCTION_POINTERS
-
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 #include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
