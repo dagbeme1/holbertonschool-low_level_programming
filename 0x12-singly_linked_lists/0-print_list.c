dc6a0a7a68dc6cd461aa457660aaeba12eaff92f
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-11-07 20:56:04 -0800

    Doing an update on print list. Using a while loop now

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,22 +3,25 @@
 #include <stdlib.h>
 
 /**
- * print_list - Print all elements of a list_t list
- * @h: Pointer to the first element of the list
- *
- * Return: Number of elements in the list
+ * print_list - prints all elements of list_t list
+ * @h: linked list
+ * Return: number of nodes in linked list
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t linkedNodes;
+	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-	linkedNodes = 1;
-	printf("[%d] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-	if (h->next != NULL)
-	linkedNodes += print_list(h->next);
+		count++;
+		h = h->next;
+	}
 
-	return (linkedNodes);
+	return (count);
 }
