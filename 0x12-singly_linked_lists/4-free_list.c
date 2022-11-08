e5e216c70d44ae247ecd1d2c3dfab3d563aeb503
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-11-07 21:01:29 -0800

    Using a while loop now

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,33 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_list - Frees a list_t list.
- * @head: A pointer to the list_t list.
+ * free_list - free linked list
+ * @head: linked list
  */
+
 void free_list(list_t *head)
 {
-	list_t *tmp;
-
-	while (head)
+	while (head != NULL)
 	{
-		tmp = head->next;
 		free(head->str);
 		free(head);
-		head = tmp;
+		head = head->next;
 	}
 }
+
+/**
+ * another way using temp pointer
+ *
+ *      list_t *ptr;
+ *
+ *	if (head == NULL) // account for no linked list
+ *		return;
+ *
+ *	while (head != NULL) // have ptr keep track of head node and free
+ *	{
+ *		ptr = head;
+ *		head = head->next; // move to next node while ptr frees prior
+ *		free(ptr->str); // free malloced strings
+ *		free(ptr);
+ *	}
+ */
