f6cacf20c16397bbb4e0e8fdcb455c4bbb9b2483
Your Name <you@example.com>
2023-08-14 22:57:26 -0500

    a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,4 +1,4 @@
-#include "search_algos.h"
+#include "search_algos.h" /* Include the header file for search algorithms */
 
 /**
  * binary_search - function that searches for a value in a sorted array of
@@ -11,28 +11,28 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-        int i = 0, start = 0, end = (int)size - 1, mid = 0;  /* Declare integer variables */
+	int i = 0, start = 0, end = (int)size - 1, mid = 0; /* Declare integer variables for counting, start, end, and mid */
 
-        if (!array)
-                return (-1);  /* Return -1 if array is NULL */
-        while (end >= start)  /* Run the binary search loop while end >= start */
-        {
-                printf("Searching in array: ");  /* Print message indicating searching in array */
-                for (i = start; i <= end; i++)  /* Loop to print elements in the current range */
-                {
-                        printf("%i", array[i]);  /* Print current array element */
-                        if (i != end)  /* Check if current element is not the last one */
-                                printf(", ");  /* Print comma and space if not the last element */
-                        else
-                                printf("\n");  /* Print newline character if last element */
-                }
-                mid = (start + end) / 2;  /* Calculate middle index of current range */
-                if (array[mid] == value)  /* Check if middle element is equal to target value */
-                        return (mid);  /* Return middle index if value is found */
-                if (value < array[mid])  /* Check if value is less than middle element */
-                        end = mid - 1;  /* Update end index to search in left half */
-                else
-                        start = mid + 1;  /* Update start index to search in right half */
-        }
-        return (-1);  /* Return -1 if value is not found */
+	if (!array)
+		return (-1); /* Return -1 if array is NULL */
+	while (end >= start) /* Run the binary search loop while end is greater than or equal to start */
+	{
+		printf("Searching in array: "); /* Print a message indicating searching in array */
+		for (i = start; i <= end; i++) /* Loop to print the elements in the current range */
+		{
+			printf("%i", array[i]); /* Print the current array element */
+			if (i != end) /* Check if the current element is not the last one in the range */
+				printf(", "); /* Print a comma and space if it's not the last element */
+			else
+				printf("\n"); /* Print a newline character if it's the last element */
+		}
+		mid = (start + end) / 2; /* Calculate the middle index of the current range */
+		if (array[mid] == value) /* Check if the middle element is equal to the target value */
+			return (mid); /* Return the middle index if the value is found */
+		if (value < array[mid]) /* Check if the value is less than the middle element */
+			end = mid - 1; /* Update the end index to search in the left half */
+		else
+			start = mid + 1; /* Update the start index to search in the right half */
+	}
+	return (-1); /* Return -1 if the value is not found */
 }
