976715f87289e8fb6a6ed7b9c75dc0d78c4a9ab0
Your Name <you@example.com>
2023-08-14 22:25:01 -0500

    a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,22 +11,22 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;  /* Initialize a counter */
+	int i = 0; /* Initialize a counter */
 
-    /* Check if the array is NULL */
-    if (!array)
-        return (-1);  /* Return -1 if array is NULL */
+	/* Check if the array is NULL */
+	if (!array)
+		return (-1); /* Return -1 if array is NULL */
 
-    /* Loop through the array */
-    for (i = 0; i < (int)size; i++, array++)
-    {
-        /* Print the value being checked in the array */
-        printf("Value checked array[%i] = [%i]\n", i, *array);
+	/* Loop through the array */
+	for (i = 0; i < (int)size; i++, array++)
+	{
+		/* Print the value being checked in the array */
+		printf("Value checked array[%i] = [%i]\n", i, *array);
 
-        /* Check if the current value matches the target value */
-        if (*array == value)
-            return (i);  /* Return the current index if value is found */
-    }
+		/* Check if the current value matches the target value */
+		if (*array == value)
+			return (i); /* Return the current index if value is found */
+	}
 
-    return (-1);  /* Return -1 if value is not found */
+	return (-1); /* Return -1 if value is not found */
 }
